881e59ee6deb3cfc14577ac2379d6d92881af7f9
AlessandroDiego <alessandro.diego.stud@ispascalcomandini.it>
2021-02-15 17:45:44 +0100

    Movimento Dell'Ascensore con Tastierino Numerico ULTIMATO

--- Proj_Ascensore/Proj_Ascensore/MainWindow.xaml.cs
+++ Proj_Ascensore/Proj_Ascensore/MainWindow.xaml.cs
@@ -25,8 +25,10 @@ namespace Proj_Ascensore
         public int posVerticaleAscensore = 600;
         public int posOrizzontaleAscensore = 294;
         public int posizioneQuarta = 0;
-        public Thread t1;
+        public Thread ThreadAndata;
+        public Thread ThreadRitorno;
         Random r1 = new Random();
+
 
 
         public MainWindow()
@@ -39,45 +41,60 @@ namespace Proj_Ascensore
 
         private void BTN_Terra_Click(object sender, RoutedEventArgs e)
         {
-            t1= new Thread(new ThreadStart(PianoTerra));
-            t1.Start();
-            Wait();
+            SpegniBottoni();
+            ThreadAndata = new Thread(new ThreadStart(PianoTerra));
+            ThreadRitorno = new Thread(new ThreadStart(Ritorno));
+            ThreadAndata.Start();
+            
+
 
         }
 
         private void BTN_1_Click(object sender, RoutedEventArgs e)
         {
-            t1 = new Thread(new ThreadStart(Piano1));
-            t1.Start();
-            Wait();
+            SpegniBottoni();
+            ThreadAndata = new Thread(new ThreadStart(Piano1));
+            ThreadRitorno = new Thread(new ThreadStart(Ritorno));
+            ThreadAndata.Start();
+            
         }
 
         private void BTN_2_Click(object sender, RoutedEventArgs e)
         {
-             t1 = new Thread(new ThreadStart(Piano2));
-            t1.Start();
-            Wait();
+            SpegniBottoni();
+            ThreadAndata = new Thread(new ThreadStart(Piano2));
+            ThreadRitorno = new Thread(new ThreadStart(Ritorno));
+            ThreadAndata.Start();
+            
+
         }
 
         private void BTN_3_Click(object sender, RoutedEventArgs e)
         {
-            t1 = new Thread(new ThreadStart(Piano3));
-            t1.Start();
-            Wait();
+            SpegniBottoni();
+            ThreadAndata = new Thread(new ThreadStart(Piano3));
+            ThreadRitorno = new Thread(new ThreadStart(Ritorno));
+            ThreadAndata.Start();
+            
         }
 
         private void BTN_4_Click(object sender, RoutedEventArgs e)
         {
-             t1 = new Thread(new ThreadStart(Piano4));
-            t1.Start();
-            Wait();
+            SpegniBottoni();
+            ThreadAndata = new Thread(new ThreadStart(Piano4));
+            ThreadRitorno = new Thread(new ThreadStart(Ritorno));
+            ThreadAndata.Start();
+            
+
         }
 
         private void BTN_5_Click(object sender, RoutedEventArgs e)
         {
-            t1 = new Thread(new ThreadStart(Piano5));
-            t1.Start();
-            Wait();
+            SpegniBottoni();
+            ThreadAndata = new Thread(new ThreadStart(Piano5));
+            ThreadRitorno = new Thread(new ThreadStart(Ritorno));
+            ThreadAndata.Start();
+            
         }
 
         public void PianoTerra()
@@ -94,8 +111,8 @@ namespace Proj_Ascensore
                 }));
 
             }
-            
         }
+
 
         public void Piano1()
         {
@@ -111,7 +128,8 @@ namespace Proj_Ascensore
                 }));
                 
             }
-           
+            Thread.Sleep(TimeSpan.FromMilliseconds(3000));
+            ThreadRitorno.Start();
 
         }
         public void Piano2()
@@ -128,7 +146,8 @@ namespace Proj_Ascensore
                 }));
 
             }
-           
+            Thread.Sleep(TimeSpan.FromMilliseconds(3000));
+            ThreadRitorno.Start();
         }
         public void Piano3()
         {
@@ -144,7 +163,8 @@ namespace Proj_Ascensore
                 }));
 
             }
-           
+            Thread.Sleep(TimeSpan.FromMilliseconds(3000));
+            ThreadRitorno.Start();
         }
         public void Piano4()
         {
@@ -160,9 +180,8 @@ namespace Proj_Ascensore
                 }));
 
             }
-            
-
-
+            Thread.Sleep(TimeSpan.FromMilliseconds(3000));
+            ThreadRitorno.Start();
         }
         public void Piano5()
         {
@@ -178,19 +197,54 @@ namespace Proj_Ascensore
                 }));
 
             }
-            
-           
+            Thread.Sleep(TimeSpan.FromMilliseconds(3000));
+            ThreadRitorno.Start();
         }
 
-        public void Wait()
+        public void Ritorno()
         {
-            Thread.Sleep(TimeSpan.FromSeconds(1));
-            posVerticaleAscensore = 600;
-            posOrizzontaleAscensore = 294;
-            posizioneQuarta = 0;
+            while (posVerticaleAscensore <= 600)
+            {
+                posVerticaleAscensore += 10;
+                posizioneQuarta -= 10;
+                Thread.Sleep(TimeSpan.FromMilliseconds(100));
+                this.Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    Ascensore_1.Margin = new Thickness(294, posVerticaleAscensore, 1049, posizioneQuarta);
+
+                }));
+            }
         }
 
+        public void SpegniBottoni()
+        {
+            BTN_Terra.IsEnabled = false;
+            BTN_1.IsEnabled = false;
+            BTN_2.IsEnabled = false;
+            BTN_3.IsEnabled = false;
+            BTN_4.IsEnabled = false;
+            BTN_5.IsEnabled = false;
+        }
+        public void AccendiBottoni()
+        {
+            BTN_Terra.IsEnabled = true;
+            BTN_1.IsEnabled = true;
+            BTN_2.IsEnabled = true;
+            BTN_3.IsEnabled = true;
+            BTN_4.IsEnabled = true;
+            BTN_5.IsEnabled = true;
+        }
 
-
+        private void Btn_Ritornato_Click(object sender, RoutedEventArgs e)
+        {
+            if (posVerticaleAscensore >= 600)
+            {
+                AccendiBottoni();
+            }
+            else
+            {
+                MessageBox.Show("L'ascensore non è ancora tornato al piano terra");
+            }
+        }
     }
 }
