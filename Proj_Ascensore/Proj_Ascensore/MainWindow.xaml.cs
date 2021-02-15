e3f0d63bbb2a236e10b02ac286bf0b40b6aa9723
AlessandroDiego <alessandro.diego.stud@ispascalcomandini.it>
2021-02-15 17:09:19 +0100

    Aggiunta Metodi Movimento con bottoni(Da Finire)

--- Proj_Ascensore/Proj_Ascensore/MainWindow.xaml.cs
+++ Proj_Ascensore/Proj_Ascensore/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Threading;
 
 namespace Proj_Ascensore
 {
@@ -20,9 +21,176 @@ namespace Proj_Ascensore
     /// </summary>
     public partial class MainWindow : Window
     {
+        readonly Uri uriAscensore = new Uri("Ascensore.png", UriKind.Relative);
+        public int posVerticaleAscensore = 600;
+        public int posOrizzontaleAscensore = 294;
+        public int posizioneQuarta = 0;
+        public Thread t1;
+        Random r1 = new Random();
+
+
         public MainWindow()
         {
             InitializeComponent();
+            ImageSource imm1 = new BitmapImage(uriAscensore);
+            Ascensore_1.Source = imm1;
+
         }
+
+        private void BTN_Terra_Click(object sender, RoutedEventArgs e)
+        {
+            t1= new Thread(new ThreadStart(PianoTerra));
+            t1.Start();
+            Wait();
+
+        }
+
+        private void BTN_1_Click(object sender, RoutedEventArgs e)
+        {
+            t1 = new Thread(new ThreadStart(Piano1));
+            t1.Start();
+            Wait();
+        }
+
+        private void BTN_2_Click(object sender, RoutedEventArgs e)
+        {
+             t1 = new Thread(new ThreadStart(Piano2));
+            t1.Start();
+            Wait();
+        }
+
+        private void BTN_3_Click(object sender, RoutedEventArgs e)
+        {
+            t1 = new Thread(new ThreadStart(Piano3));
+            t1.Start();
+            Wait();
+        }
+
+        private void BTN_4_Click(object sender, RoutedEventArgs e)
+        {
+             t1 = new Thread(new ThreadStart(Piano4));
+            t1.Start();
+            Wait();
+        }
+
+        private void BTN_5_Click(object sender, RoutedEventArgs e)
+        {
+            t1 = new Thread(new ThreadStart(Piano5));
+            t1.Start();
+            Wait();
+        }
+
+        public void PianoTerra()
+        {
+            while (posVerticaleAscensore > 600)
+            {
+                posVerticaleAscensore -= 10;
+                posizioneQuarta += 10;
+                Thread.Sleep(TimeSpan.FromMilliseconds(100));
+                this.Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    Ascensore_1.Margin = new Thickness(294, posVerticaleAscensore, 1049, posizioneQuarta);
+
+                }));
+
+            }
+            
+        }
+
+        public void Piano1()
+        {
+            while (posVerticaleAscensore > 490)
+            {
+                posVerticaleAscensore -= 10;
+                posizioneQuarta += 10;
+                Thread.Sleep(TimeSpan.FromMilliseconds(100));
+                this.Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    Ascensore_1.Margin = new Thickness(294, posVerticaleAscensore, 1049, posizioneQuarta);
+                   
+                }));
+                
+            }
+           
+
+        }
+        public void Piano2()
+        {
+            while (posVerticaleAscensore > 372)
+            {
+                posVerticaleAscensore -= 10;
+                posizioneQuarta += 10;
+                Thread.Sleep(TimeSpan.FromMilliseconds(100));
+                this.Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    Ascensore_1.Margin = new Thickness(294, posVerticaleAscensore, 1049, posizioneQuarta);
+
+                }));
+
+            }
+           
+        }
+        public void Piano3()
+        {
+            while (posVerticaleAscensore > 246)
+            {
+                posVerticaleAscensore -= 10;
+                posizioneQuarta += 10;
+                Thread.Sleep(TimeSpan.FromMilliseconds(100));
+                this.Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    Ascensore_1.Margin = new Thickness(294, posVerticaleAscensore, 1049, posizioneQuarta);
+
+                }));
+
+            }
+           
+        }
+        public void Piano4()
+        {
+            while (posVerticaleAscensore > 132)
+            {
+                posVerticaleAscensore -= 10;
+                posizioneQuarta += 10;
+                Thread.Sleep(TimeSpan.FromMilliseconds(100));
+                this.Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    Ascensore_1.Margin = new Thickness(294, posVerticaleAscensore, 1049, posizioneQuarta);
+
+                }));
+
+            }
+            
+
+
+        }
+        public void Piano5()
+        {
+            while (posVerticaleAscensore > 0)
+            {
+                posVerticaleAscensore -= 10;
+                posizioneQuarta += 10;
+                Thread.Sleep(TimeSpan.FromMilliseconds(100));
+                this.Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    Ascensore_1.Margin = new Thickness(294, posVerticaleAscensore, 1049, posizioneQuarta);
+
+                }));
+
+            }
+            
+           
+        }
+
+        public void Wait()
+        {
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+            posVerticaleAscensore = 600;
+            posOrizzontaleAscensore = 294;
+            posizioneQuarta = 0;
+        }
+
+
+
     }
 }
